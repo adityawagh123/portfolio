ce2ec2212a86523a540aa5b2dee64c4fa1bb947d
adityawagh123 <adityawagh875@gmail.com>
2026-02-28 20:48:53 +0530

    second commit

--- src/main/java/com/aditya/portfolio/PortfolioApplication.java
+++ src/main/java/com/aditya/portfolio/PortfolioApplication.java
@@ -8,7 +8,7 @@ public class PortfolioApplication {
 
 	public static void main(String[] args) {
 		SpringApplication.run(PortfolioApplication.class, args);
-		System.out.println("Welcome");
+		System.out.println("Welcome aditya");
 	}
 
 }
